7131112cad969eef80a8fedc35ad9ee76990455c
Anton Kochnev <anton.kochnev@gmail.com>
2014-09-30 09:57:39 +0900

    update static url

--- smyttest/settings.py
+++ smyttest/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/smyttest/static/'
 STATICFILES_DIRS = ('static',)
 
 TEMPLATE_DIRS = ('html',)
